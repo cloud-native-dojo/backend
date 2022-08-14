cb9a7dac8b53dbeee2a0d393b95afa45b558563b
tasyumina-hito <48035533+tasyumina-hito@users.noreply.github.com>
2022-08-14 16:01:41 +0900

    run helm from python

--- k8s.py
+++ k8s.py
@@ -1,66 +1,15 @@
 import time
 
-from kubernetes import config
-from kubernetes.client import Configuration
-from kubernetes.client.api import core_v1_api
-from kubernetes.client.rest import ApiException
-from kubernetes.stream import stream
-
-
-def exec_commands(api_instance):
-    name = 'busybox-test'
-    resp = None
-    try:
-        resp = api_instance.read_namespaced_pod(name=name,
-                                                namespace='sample')
-    except ApiException as e:
-        if e.status != 404:
-            print("Unknown error: %s" % e)
-            exit(1)
-
-    if not resp:
-        print("Pod %s does not exist. Creating it..." % name)
-        pod_manifest = {
-            'apiVersion': 'v1',
-            'kind': 'Pod',
-            'metadata': {
-                'name': name
-            },
-            'spec': {
-                'containers': [{
-                    'image': 'busybox',
-                    'name': 'sleep',
-                    "args": [
-                        "/bin/sh",
-                        "-c",
-                        "while true;do date;sleep 5; done"
-                    ]
-                }]
-            }
-        }
-        resp = api_instance.create_namespaced_pod(body=pod_manifest,
-                                                  namespace='sample')
-
-        while True:
-            resp = api_instance.read_namespaced_pod(name=name,
-                                                    namespace='sample')
-            if resp.status.phase != 'Pending':
-                break
-            time.sleep(1)
-        print("Done.")
-
+import subprocess, sys
 
 def main():
-    config.load_kube_config()
-    try:
-        c = Configuration().get_default_copy()
-    except AttributeError:
-        c = Configuration()
-        c.assert_hostname = False
-    Configuration.set_default(c)
-    core_v1 = core_v1_api.CoreV1Api()
-
-    exec_commands(core_v1)
+    cp = subprocess.run(['C:/Program Files/helm/helm','--set', 'service.type=NodePort', 'install', 'bitnami/wordpress', '--generate-name'])
+    if cp.returncode != 0:
+        print('ls failed.', file=sys.stderr)
+        sys.exit(1)
+    
+    cp = subprocess.run(['kubectl', 'get', 'svc'], encoding='utf-8', stdout=subprocess.PIPE)
+    print(f'{cp.stdout}')
 
 
 if __name__ == '__main__':
