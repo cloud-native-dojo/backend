c4948992e196de819bfa527f547ddae9b12d1f37
tasyumina-hito <48035533+tasyumina-hito@users.noreply.github.com>
2022-08-14 10:00:48 +0900

    test official sample

--- k8s.py
+++ k8s.py
@@ -1,11 +1,67 @@
-from kubernetes import client, config, watch
-import yaml
+import time
 
-# Configs can be set in Configuration class directly or using helper utility
-config.load_kube_config()
+from kubernetes import config
+from kubernetes.client import Configuration
+from kubernetes.client.api import core_v1_api
+from kubernetes.client.rest import ApiException
+from kubernetes.stream import stream
 
-with open("pod.yaml", "r",encoding="utf-8_sig") as f:
-    pod = yaml.safe_load(f)
-    k8s_apps_v1 = client.CoreV1Api()
-    resp = k8s_apps_v1.create_namespaced_pod(body=pod, namespace="sample")
-    print("pod created. status='%s'" % resp.metadata.name)
+
+def exec_commands(api_instance):
+    name = 'busybox-test'
+    resp = None
+    try:
+        resp = api_instance.read_namespaced_pod(name=name,
+                                                namespace='sample')
+    except ApiException as e:
+        if e.status != 404:
+            print("Unknown error: %s" % e)
+            exit(1)
+
+    if not resp:
+        print("Pod %s does not exist. Creating it..." % name)
+        pod_manifest = {
+            'apiVersion': 'v1',
+            'kind': 'Pod',
+            'metadata': {
+                'name': name
+            },
+            'spec': {
+                'containers': [{
+                    'image': 'busybox',
+                    'name': 'sleep',
+                    "args": [
+                        "/bin/sh",
+                        "-c",
+                        "while true;do date;sleep 5; done"
+                    ]
+                }]
+            }
+        }
+        resp = api_instance.create_namespaced_pod(body=pod_manifest,
+                                                  namespace='sample')
+
+        while True:
+            resp = api_instance.read_namespaced_pod(name=name,
+                                                    namespace='sample')
+            if resp.status.phase != 'Pending':
+                break
+            time.sleep(1)
+        print("Done.")
+
+
+def main():
+    config.load_kube_config()
+    try:
+        c = Configuration().get_default_copy()
+    except AttributeError:
+        c = Configuration()
+        c.assert_hostname = False
+    Configuration.set_default(c)
+    core_v1 = core_v1_api.CoreV1Api()
+
+    exec_commands(core_v1)
+
+
+if __name__ == '__main__':
+    main()
