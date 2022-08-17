d9cfc31697a099d2e4a383bd0f22d9fdd233f75d
Tomoyuki KOYAMA <tmyk.kym@gmail.com>
2022-08-17 23:25:42 +0900

    update README

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # backend
+
+## 動かし方
+
+仮想環境を適宜つくる．
+
+```
+python -m venv .venv
+. .venv/bin/activate
+```
+
+パッケージを入れる．
+
+```
+pip install fastapi uvicorn
+```
+
+起動する．
+
+```
+uvicorn main:app --host 0.0.0.0
+```
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Dict
-from kubernetes import client, config
 import k8s
 
 app = FastAPI()
 
+
 class Pod(BaseModel):
     containers: Dict[str, int]
+
 
 class Service(BaseModel):
     port: int
     name: str
+
 
 @app.get("/")
 def read_root():
@@ -26,27 +28,31 @@ def create_pod(pod: Pod):
         if container[0] == "wordpress":
             callback = k8s.make_pod()
 
-
     return {"name": callback}
+
 
 @app.get("/pods/")
 def get_pod():
     return {"pods": k8s.get_pod()}
 
+
 @app.post("/services/")
 def create_service(service: Service):
-    callback = k8s.change_port(service.name,service.port)
+    callback = k8s.change_port(service.name, service.port)
     return {"result": callback}
+
 
 @app.get("/ports_suggest/")
 def get_unused_port():
     ports = k8s.get_used_port()
-    return {"sugessted_port":ports}
+    return {"sugessted_port": ports}
+
 
 @app.delete("/pods/{name}")
 def delete_pod():
     callback = k8s.delete_pod(name)
     return {"deleted": callback}
+
 
 @app.delete("/services/{name}")
 def delete_pod(name: str):
