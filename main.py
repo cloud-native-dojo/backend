9c2dd890be9959057de02ec88afced76a9e866c8
tasyumina-hito <48035533+tasyumina-hito@users.noreply.github.com>
2022-08-21 10:55:44 +0900

    fix

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict
 import k8s
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,  
+    allow_methods=["*"],      
+    allow_headers=["*"]      
+)
 
 
 class Pod(BaseModel):
